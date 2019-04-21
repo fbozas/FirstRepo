7abb859c061148cb116f224a086f88f21766f40c
Panos Bozas <fbozas@hotmail.com>
2019-04-21 19:35:27 +0300

    changed printed text

--- FirstRepo/Program.cs
+++ FirstRepo/Program.cs
@@ -10,7 +10,7 @@ namespace FirstRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello maroco");
         }
     }
 }
